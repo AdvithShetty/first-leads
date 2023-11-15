e8e6a5745087656d7daf97273056ac8627470520
Aniruddha Sil <welcometohellv@gmail.com>
2023-11-16 01:19:44 +0530

    refactor(select leads): :art: route to signin if no refreshtoken

--- src/app/onboarding/select-leads/layout.tsx
+++ src/app/onboarding/select-leads/layout.tsx
@@ -1,8 +1,27 @@
 'use client'
+import LoadingUi from '@/components/LoadingUi'
+import useRefreshToken from '@/hooks/useRefreshToken'
+import useUser from '@/hooks/useUser'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import { useIsClient } from 'usehooks-ts'
 import { CartSidebarProvider } from './CartSidebarContext'
 import CartWrapper from './CartWrapper'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { data: user, isLoading, isSuccess } = useUser()
+  const isClient = useIsClient()
+  const router = useRouter()
+  const { refreshToken } = useRefreshToken()
+
+  useEffect(() => {
+    if (!refreshToken) {
+      router.push('/signin')
+    }
+  }, [refreshToken, router])
+
+  if ((isLoading && !isSuccess) || !isClient) return <LoadingUi />
+
   return (
     <CartSidebarProvider>
       <div className='relative grid min-h-screen w-full grid-cols-4 bg-white'>
